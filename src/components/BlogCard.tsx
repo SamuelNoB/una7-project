a91d10d7253cf993ccc30b039f745ab996fb18bc
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-05-08 23:23:02 -0300

    card de postagem criado

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { Card, CardBody, CardImg, CardTitle, CardSubtitle, CardText, CardFooter } from "reactstrap";
+
+function BlogCard(params: any) {
+
+  return (
+    
+      <Card
+      color="light"
+        >
+        <CardImg
+        src={'/images/mountain.jpeg'}
+        top
+        width={'100%'}
+        />
+        <CardBody>
+          <CardTitle tag={'h4'}>
+            Blog Post
+          </CardTitle>
+          <br />
+          <br />
+      
+          <CardText className="text-muted">Postado Em: 22/04/2022</CardText>
+        </CardBody>
+      </Card>
+  );
+}
+
+export default BlogCard;
