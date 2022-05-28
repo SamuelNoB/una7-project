e9184a3ba5e377d452980ff7198ed82014e08e0c
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-05-28 17:12:22 -0300

    implementando insercao de publicacao

--- src/pages/api/post/create.ts
+++ src/pages/api/post/create.ts
@@ -1,18 +1,55 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import nextConnect from 'next-connect'
+import { PrismaClient, Publication } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type RequestPayload = {
-
-}
+import multerUpload from '../../../core/multerConfig'
 
 type Data = {
-  name: string
+  message: string
+  data: Publication
 }
+type Error = {
+  error: string
+}
+const apiRoute = nextConnect({
+  onNoMatch(req: NextApiRequest,
+    res: NextApiResponse<Data | Error>
+  ) {  return res.status(400).json({
+    error: 'Ocorreu um erro ao processar a solicitação.'
+  })}
+})
+
+const prisma = new PrismaClient()
 
 
-export default function handler(
+async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Error>
 ) {
-  
+  const data: createPostInput = req.body;
+  data.coverImage = req.file.filename
+    const result = await prisma.publication.create({
+      data: {
+        content: data.content,
+        subTitle: data.subtitle,
+        title: data.title,
+        active: data.active,
+        coverImage: data.coverImage ?? ''
+      }
+    });
+    
+  return res.status(200).json({
+    message: "Publicação criada com sucesso",
+    data: result
+  })
 }
+
+const uploadMiddleware = multerUpload.single('coverImage')
+apiRoute.use(uploadMiddleware)
+apiRoute.post(handler)
+export default apiRoute
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
