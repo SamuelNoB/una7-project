eee9b969ef7c61b251e885a6086e9e587e675ae1
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-05-28 17:12:42 -0300

    criando rotas de obtencao de publicacao

---
+++ src/pages/api/post/get/[id].ts
@@ -0,0 +1,41 @@
+import { PrismaClient, Publication } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import nextConnect from 'next-connect'
+
+type Data = {
+  message: string
+  data?: Publication
+}
+type Error = {
+  error: string
+}
+const apiRoute = nextConnect({
+  onNoMatch(req: NextApiRequest,
+    res: NextApiResponse<Data | Error>
+  ) {  return res.status(400).json({
+    error: 'Ocorreu um erro ao processar a solicitação.'
+  })}
+})
+const prisma = new PrismaClient()
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let {id}: any = req.query
+  id = Number(id)
+  const result = await prisma.publication.findUnique({
+    where: {
+      id
+    }
+  });
+  if (result) {
+    return res.status(400).json({
+      error: 'não foi encontrado nenhuma publicaçao'
+    });
+  }
+  return res.status(200).json({
+    message: 'Publicação encontrada',
+    data: result
+  });
+}
+
+apiRoute.get(handler)
+export default apiRoute
---
+++ src/pages/api/post/get/index.ts
@@ -0,0 +1,28 @@
+import { PrismaClient,Publication } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import nextConnect from 'next-connect'
+
+type Data = {
+  posts: Publication[]
+}
+type Error = {
+  error: string
+}
+
+const apiRoute = nextConnect({
+  onNoMatch(req: NextApiRequest,
+    res: NextApiResponse<Data | Error>
+  ) {  return res.status(400).json({
+    error: 'Ocorreu um erro ao processar a solicitação.'
+  })}
+})
+
+const prisma = new PrismaClient()
+
+async function getAllPosts(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const allPosts = await prisma.publication.findMany();
+  return res.status(200).json({posts: allPosts})
+}
+
+apiRoute.get(getAllPosts)
+export default apiRoute;
