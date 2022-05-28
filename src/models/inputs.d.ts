ce044e7e832b3f551d7f276c68ea225c29571d30
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-05-28 17:11:32 -0300

    criando inputs

--- src/models/inputs.d.ts
+++ src/models/inputs.d.ts
@@ -1,6 +1,38 @@
+
 type ContactInput = {
   fullName: string,
   email: string,
   subject: string,
   message: string,
+}
+
+type createPostInput = {
+  active: boolean,
+  title: string,
+  coverImage?: string,
+  subtitle: string,
+  content: string,
+}
+
+type updatePostInput = {
+  active?: Boolean,
+  title?: string,
+  subtitle?: string,
+  content?: string,
+}
+type updatePostBody = {
+  id: number,
+  body: updatePostBody
+}
+
+type createPartnerInput = {
+  name: string,
+  link: string,
+  active: boolean,
+}
+
+type createClientInput = {
+  visible: boolean,
+  name: string,
+  link: string
 }
