f16eeeafecf9ed0faf8fa0ebda453be3ae46d32a
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-06-13 22:02:46 -0300

    Add botão de compartilhamento

--- src/pages/blog/post/[id].tsx
+++ src/pages/blog/post/[id].tsx
@@ -8,6 +8,7 @@ import { Col, Row } from "reactstrap"
 import parse from 'html-react-parser';
 import Image from "next/image"
 import dayjs from "dayjs"
+import {AiOutlineLink} from "react-icons/ai"
 
 function BlogIndex(props: any) {
   const [blogContent, setBlogContent] = useState<Publication>({
@@ -33,23 +34,40 @@ function BlogIndex(props: any) {
     }
   }, [data])
 
+  function sharePost() {
+    navigator.share({
+      text: blogContent?.subTitle,
+      title: blogContent.title,
+      url: router.asPath
+    })
+  }
+
   return (
     <div className="post-container">
-      <Row className="justify-content-between">
-        <Col xs='auto' className="text-muted">Criado em: {displayDate}</Col>
-        <Col xs='auto'>bla bla</Col>
-      </Row>
-      <Row>
-        <Col lg={12}>
-          <h1>{blogContent?.title}</h1>
-        </Col>
-        <Col lg={12}>
-          <h3 className="text-muted">{blogContent.subTitle}</h3>
-        </Col>
-        <Col>
-        <Image src={`data:${blogContent.imageType};base64, ${blogContent.coverImage}`} alt="client" width={'100%'} height={'100%'} layout='responsive' />
-        </Col>
-      </Row>
+      <div className="post-container__content">
+        <Row className="justify-content-between">
+          <Col xs='auto' className="text-muted">Criado em: {displayDate}</Col>
+          <Col xs='auto'>
+          <Row className="d-flex text-muted">
+            <Col xs='auto' style={{paddingRight: 0}}>Compartilhar:</Col>
+            <Col xs='auto' className="hoverClassForButtons text-muted"  href="https://www.facebook.com/agenciauna7" target="_blank">
+              <AiOutlineLink size={25} onClick={ ()=> sharePost()}/>
+            </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={12}>
+            <h1>{blogContent?.title}</h1>
+          </Col>
+          <Col lg={12}>
+            <h3 className="text-muted">{blogContent.subTitle}</h3>
+          </Col>
+          <Col>
+          <Image src={`data:${blogContent.imageType};base64, ${blogContent.coverImage}`} alt="client" width={'100%'} height={'100%'} layout='responsive' />
+          </Col>
+        </Row>
+      </div>
       {
         blogContent?.content ? parse(blogContent?.content as string) : (<></>)
       }
