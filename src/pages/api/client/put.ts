61bbe9bdbedefab9a37d78caa92aacd5ac6a62bf
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-06-26 12:20:37 -0300

    Add rota de put cliente

--- src/pages/api/client/put.ts
+++ src/pages/api/client/put.ts
@@ -1,13 +1,15 @@
 import nextConnect from 'next-connect'
 import { getSession } from "next-auth/react"
-import { Publication } from '@prisma/client'
+import { Client } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import multiparty from 'multiparty';
 import {convertImage} from '../../../core/imageConverter';
 import prisma from '@core/db'
+
+
 type Data = {
   message: string
-  data: Publication
+  data: Client
 }
 type Error = {
   error: string
@@ -20,65 +22,60 @@ const apiRoute = nextConnect({
   })}
 })
 
+
 type promiseDataInput = {
-  id: number
+  id: number,
   data: any,
   image: any
 }
-
-async function handler(
+async function updateClient(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
   const session = await getSession({ req })
-
   if( !session) {
     return res.status(401).json({
       error: 'Você não pode acessar essa rota'
     })
   }
-  const user = await prisma.user.findUnique( {
-    where: {
-      email: session.user?.email as any
-    }
-  })
-
   const form = new multiparty.Form({autoFields: true, autoFiles: true});
+
   const {id, data, image} = await new Promise<promiseDataInput>((resolve, rejects) => {
     form.parse(req, (err, fields, files) => {
       const parsedData: any = {
-        active: fields.active[0],
-        title: fields.title[0],
-        subTitle: fields.subtitle[0],
-        content: fields.content[0],
+        visible: fields.visible[0],
+        name: fields.name[0],
+        link: fields.link[0],
       }
       if (files.Image) {
-        parsedData.imageType = files.Image[0].headers['content-type']
-        parsedData.coverImage = files.Image[0]
+        parsedData.clientPhoto = files.Image[0]
+        parsedData.photoType = files.Image[0].headers['content-type']
       }
-      resolve({id: Number(fields.id[0]), data: parsedData, image: parsedData.coverImage});
+      resolve({id: Number(fields.id[0]),data: parsedData ,image: files.Image[0]});
     });
   });
 
-  if (data.coverImage) {
-    data.coverImage = await convertImage(image.path)
+  if (data.clientPhoto) {
+    data.clientPhoto = await convertImage(image.path)
   }
-  data.active = data.active === 'true' ? true : false;
 
-    const result = await prisma.publication.update({
+  data.visible = data.visible === 'true' ? true : false;
+    const result = await prisma.client.update({
       where: {
-        id
+        id: id
       },
       data
     });
 
   return res.status(200).json({
-    message: "Publicação atualizada com sucesso",
+    message: "Cliente criado com sucesso",
     data: result
   })
 }
 
-apiRoute.put(handler)
+//const uploadMiddleware = multerUpload.single('Image')
+//apiRoute.use(uploadMiddleware)
+apiRoute.put(updateClient)
 export default apiRoute
 
 export const config = {
